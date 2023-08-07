75cccbb6f79735645ef588db361e4e169fc11e70
yxsicd <yxsicd@users.noreply.github.com>
2023-08-08 00:04:44 +0800

    Update web_demo.py

--- web_demo.py
+++ web_demo.py
@@ -3,11 +3,11 @@ import gradio as gr
 import mdtex2html
 from utils import load_model_on_gpus
 
-tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True)
-model = AutoModel.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True).quantize(8).cuda()
+tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b-32k", trust_remote_code=True)
+# model = AutoModel.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True).quantize(8).cuda()
 # 多显卡支持，使用下面两行代替上面一行，将num_gpus改为你实际的显卡数量
 # from utils import load_model_on_gpus
-# model = load_model_on_gpus("THUDM/chatglm2-6b", num_gpus=2)
+model = load_model_on_gpus("THUDM/chatglm2-6b-32k", num_gpus=2).quantize(8).cuda()
 model = model.eval()
 
 """Override Chatbot.postprocess"""
