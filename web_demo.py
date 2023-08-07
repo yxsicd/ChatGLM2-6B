4fdeccc63fab1ca3d99d496e3fbc4ec35bcdd04d
yxsicd <yxsicd@users.noreply.github.com>
2023-08-08 00:14:27 +0800

    Update web_demo.py

--- web_demo.py
+++ web_demo.py
@@ -7,7 +7,7 @@ tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b-32k", trust_remote_
 # model = AutoModel.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True).quantize(8).cuda()
 # 多显卡支持，使用下面两行代替上面一行，将num_gpus改为你实际的显卡数量
 # from utils import load_model_on_gpus
-model = load_model_on_gpus("THUDM/chatglm2-6b-32k", num_gpus=2).quantize(8).cuda()
+model = load_model_on_gpus("THUDM/chatglm2-6b-32k", num_gpus=2).cuda()
 model = model.eval()
 
 """Override Chatbot.postprocess"""
